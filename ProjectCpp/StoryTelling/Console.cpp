d559fd80be6b5a7cf33af48ee29af9e34d136415
gydud0819 <pstar0819@naver.com>
2025-05-06 21:43:09 +0900

    (2025-05-06) 스토리텔링 게임 구현 추가 수정 완료

--- ProjectCpp/StoryTelling/Console.cpp
+++ ProjectCpp/StoryTelling/Console.cpp
@@ -1,1 +1,16 @@
 #include "Console.h"
+
+void Console::GotoXY(int x, int y)
+{
+	COORD pos = { static_cast<short>(x), static_cast<short>(y) };	//  static_cast<short>(x), static_cast<short>(y) 자료구조때 문법 배울예정
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+void Console::CursorVisible(bool show)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cursorinfo;
+	GetConsoleCursorInfo(hConsole, &cursorinfo);
+	cursorinfo.bVisible = show;
+	SetConsoleCursorInfo(hConsole, &cursorinfo);
+}
--- ProjectCpp/StoryTelling/main.cpp
+++ ProjectCpp/StoryTelling/main.cpp
@@ -27,6 +27,7 @@ int main()
 {
 	ImageModel model;
 	Actor actor;
+
 	/*Console::GotoXY(10, 1);
 	std::cout << model.baseballPlayer << std::endl;*/
 
@@ -34,53 +35,57 @@ int main()
 
 	Image baseball(model.baseballPlayer, 50);
 	Console::GotoXY(20, 20);
-	actor.Tell("나레이션", "타자가 타석에 들어선다.");	// 대사
 
 	baseball.move(0, 5, 10, 500);		// 플레이어 애니메이션
-	actor.Tell("나레이션", "타자가 준비를 한다.");
+	actor.Tell("나레이션", "9회말 2아웃 2사 2, 3루 5:4 홈팀이 지고 있는 상황.");	// 대사
+	actor.Tell("나레이션", "타자가 타석에 들어서고 타격 준비를 한다.");	// 대사
 	actor.Tell("나레이션", "타자는 어떤 선택을 할까요?");
 	bool b1 = actor.Selection("타자", "초구를 지켜본다.", "방망이를 휘두른다.");
-	bool b2 = actor.Selection("타자", "공을 지켜본다.", "방망이를 휘두른다.");
 
-
-	if (b1)
+	while (true)
 	{
-		cout << "초구를 지켜보았다." << endl;
-		actor.Tell("나레이션", "초구는 스트라이크이다.");
+		if (b1)
+		{
+			cout << "초구를 지켜보았다." << endl;
+			actor.Tell("나레이션", "초구는 스트라이크이다.");
 
-		/*actor.Tell("나레이션", "타자는 어떤 선택을 할까요?");
-		bool b2 = actor.Selection("타자", "공을 지켜본다.", "방망이를 휘두른다.");*/
+		}
+		if (b1)
+		{
+			actor.Tell("나레이션", "타자는 어떤 선택을 할까요?");
+			bool b2 = actor.Selection("타자", "공을 지켜본다.", "배트를 휘두른다.");
 
-		//bool b2 = actor.Selection("타자", "공을 지켜본다.", "방망이를 휘두른다.");
+			cout << "방망이를 휘둘렀다." << endl;
+			actor.Tell("나레이션", "두번째 공도 스트라이크이다.");
+		}
+		if (b1)
+		{
+			actor.Tell("나레이션", "타자는 어떤 선택을 할까요?");
+			bool b2 = actor.Selection("타자", "공을 지켜본다.", "배트를 휘두른다.");
+
+			cout << "타자는 배트를 휘둘렀다." << endl;
+			actor.Tell("나레이션", "세번째는 헛스윙이다.");
+		}
+		if (b1)
+		{
+			actor.Tell("나레이션", "타자는 어떤 선택을 할까요?");
+			bool b2 = actor.Selection("타자", "공을 지켜본다.", "배트를 휘두른다.");
+
+			cout << "타자는 배트를 휘둘렀다." << endl;
+			actor.Tell("나레이션", "네번째 공은 파울이다.");
+		}
+		if (b1)
+		{
+			actor.Tell("나레이션", "타자는 어떤 선택을 할까요?");
+			bool b2 = actor.Selection("타자", "공을 지켜본다.", "배트를 휘두른다.");
+
+			cout << "타자는 배트를 휘둘렀다." << endl;
+			actor.Tell("나레이션", "다섯번 째 공이 좌익수 담장 뒤로 넘어간다.");
+		}
+
+		cout << "공이 담장을 넘어가며 역전 쓰리런 홈런을 쳤다.." << endl;
+		actor.Tell("나레이션", "9회말 5:7로 역전을 하면서 홈팀이 경기를 이겼다.");
+
+		break;
 	}
-	if (b2)
-	{
-		actor.Tell("나레이션", "타자는 어떤 선택을 할까요?");
-		bool b2 = actor.Selection("타자", "공을 지켜본다.", "방망이를 휘두른다.");
-
-		cout << "방망이를 휘둘렀다." << endl;
-		actor.Tell("나레이션", "두번째 공도 스트라이크이다.");
-	}
-	if (b2)
-	{
-		actor.Tell("나레이션", "타자는 어떤 선택을 할까요?");
-		bool b2 = actor.Selection("타자", "공을 지켜본다.", "방망이를 휘두른다.");
-
-		cout << "방망이를 휘둘렀다." << endl;
-		actor.Tell("나레이션", "세번째 공은 파울이다.");
-	}
-	if (b2)
-	{
-		actor.Tell("나레이션", "타자는 어떤 선택을 할까요?");
-		bool b2 = actor.Selection("타자", "공을 지켜본다.", "방망이를 휘두른다.");
-
-		cout << "방망이를 휘둘렀다." << endl;
-		actor.Tell("나레이션", "네번째 공이 담장을 넘어간다. 홈런을 쳤다.");
-	}
-
-
-
-	//baseball.moveReverse(10, 5, 15, 500);
-
-	while (true);
 }
